179c20ee2bc1527cbd98370ea2ffdb0cf3ff2194
Jonas Diete <jonas.diete@gmail.com>
2022-08-10 14:23:28 +0000

    added code to round up too

--- lib/closest_multiple.rb
+++ lib/closest_multiple.rb
@@ -2,5 +2,7 @@ def closest_multiple(number)
   last_digit = number.to_s[-1]
   if  last_digit.to_i < 5
     return number - last_digit.to_i
+  else
+    return number - last_digit.to_i + 10
   end
 end

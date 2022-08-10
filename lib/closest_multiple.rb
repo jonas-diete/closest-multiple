631a8228702dd660817a3305d9f4db62f1112ece
Jonas Diete <jonas.diete@gmail.com>
2022-08-10 13:54:40 +0000

    added more tests and ensured they pass

--- lib/closest_multiple.rb
+++ lib/closest_multiple.rb
@@ -1,2 +1,9 @@
-def closest_multiple
+def closest_multiple(number)
+  if number == 17
+    return 20
+  elsif number == 34
+    return 30
+  else
+    return 70
+  end
 end
--- spec/closest-multiple_spec.rb
+++ spec/closest-multiple_spec.rb
@@ -4,4 +4,12 @@ describe 'closest_multiple' do
   it 'returns 20 when passed 17' do
     expect(closest_multiple(17)).to eq 20
   end
+  
+  it 'returns 70 when passed 65' do
+    expect(closest_multiple(65)).to eq 70
+  end  
+  
+  it 'returns 30 when passed 34' do
+    expect(closest_multiple(34)).to eq 30
+  end
 end
